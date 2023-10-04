ac0dad53c1408f0ec418f176d56ef243f9279e07
mohammedrahamadulla <rahamadulla786.md@gmail.com>
2023-10-04 16:36:24 +0530

    updated server ip

--- jfrog.py
+++ jfrog.py
@@ -5,8 +5,8 @@ import requests
 
 # Call the 'upload_to_artifactory
 def jfrogUpload():
-    url = 'http://3.85.110.208:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
-    jar_path = '/var/lib/jenkins/workspace/cicd/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    url = 'http://3.89.246.247:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    jar_path = '/var/lib/jenkins/workspace/CICD/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
     username = 'admin'
     password = 'India@123'
     
--- jfrog1.py
+++ jfrog1.py
@@ -1,47 +1,27 @@
+# Import libraries
 import requests
+import json
 
-def jfrogupload():
-    local_jar_path = "/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
-    artifactory_url = "http://34.201.127.250:8082/artifactory/example-repo-local/"
-    username = "admin"
-    password = "India@123"
-    with open(local_file_path, 'rb') as file:
-        response = requests.put(artifactory_url,data=file,auth=(username, password))
-            
-        if response.status_code == 201:
-            print(f"Successfully uploaded {local_file_path} to {artifactory_url}")
-        else:
-            print(f"Failed to upload {local_file_path}. Response: {response.status_code} - {response.text}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#!/usr/bin/env python3
-
-import requests
-
-def jfrogUpload():
-    url = 'http://34.201.127.250:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
-    jar_path = '/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
-    username = 'admin'
-    password = 'India@123'
-    with open(jar_path, 'rb') as file:
-        response = requests.put(url, auth=(username,password), data=file)
-
-        if response.status_code == 201:
-            print(f"\nSuccessfully uploaded!")
-        else:
-            print(f"PUT request failed with status code {response.status_code}")
-            
+# Call the 'upload_to_artifactory' function using the loaded configuration values.
+def jfrog_artifactory(jar_path, artifactory_url, username, password):
+    # Retrieve individual configuration values using the respective keys.
+    jar_path = config["jar_path"]
+    artifactory_url = config["artifactory_url"]
+    username = config["username"]
+    password = config["password"]
+    
+    # Load configurations from the external file 'config.json'.
+    # This allows the configuration to be separated from the main code.
+    with open("config.json", "r") as config_file:
+         # Parse the JSON content of the configuration file into a Python dictionary.
+         config = json.load(config_file)
+        
+         # Send an HTTP PUT request to the specified Artifactory URL.
+         response = requests.put(artifactory_url,data=config_file,auth=(username, password))
+        
+         # Check the HTTP response status to see if the upload was successful.
+         if response.status_code == 201:
+             print(f"Successfully uploaded {jar_path} to {artifactory_url}")
+         else:
+             # Print an error message if the upload failed.
+             print(f"Failed to upload {jar_path}. Response: {response.status_code} - {response.text}")
